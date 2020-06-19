9a976bad44fb670341a532d24bce2eeb1550a16e
amin havsi <pau.ahq@gmail.com>
2020-06-19 20:05:14 +0430

    some change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import NotFound from './components/common/notFound';
+import HomePage from './components/HomePage';
 
 function App() {
     return (
         <React.Fragment>
             <Switch>
+                <Route exact path="/" component={HomePage} />
                 <Route path="/notFound" component={NotFound} />
                 <Redirect to="/notFound" />
             </Switch>
