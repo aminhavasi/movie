f4848921be353b880c622ba298555d94911f76e1
amin havsi <pau.ahq@gmail.com>
2020-06-21 17:39:06 +0430

    image resize

--- src/components/nowPlay.js
+++ src/components/nowPlay.js
@@ -1,5 +1,34 @@
 import React from 'react';
 const NowPlay = () => {
-    return <div>sd</div>;
+    return (
+        <React.Fragment>
+            <div className="row bg-danger col-12">
+                <div className="mx-auto img-fluid col-3">
+                    <img
+                        className="img"
+                        src={require('./../resource/img/test.jpg')}
+                    />
+                </div>
+                <div className="mx-auto img-fluid col-3">
+                    <img
+                        className="img"
+                        src={require('./../resource/img/test.jpg')}
+                    />
+                </div>{' '}
+                <div className="mx-auto img-fluid col-3">
+                    <img
+                        className="img"
+                        src={require('./../resource/img/test.jpg')}
+                    />
+                </div>{' '}
+                <div className="mx-auto img-fluid col-3">
+                    <img
+                        className="img"
+                        src={require('./../resource/img/test.jpg')}
+                    />
+                </div>
+            </div>
+        </React.Fragment>
+    );
 };
 export default NowPlay;
