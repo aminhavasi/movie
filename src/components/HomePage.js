dbeb1aa48c4b015e0137d41d920928d033cfd2be
amin havsi <pau.ahq@gmail.com>
2020-06-22 17:29:43 +0430

    home page after adding footer and comming soon and this week movie updated

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from './navbar';
 import NowPlay from './nowPlay';
+import WeekMovie from './thisWeekMovies';
+import ComingSoon from './comingSoon';
+import Footer from './common/footer';
 const HomePage = () => {
     return (
         <div>
@@ -9,7 +12,15 @@ const HomePage = () => {
                 <div className="container">
                     <h5>NOW PLAYING</h5>
                     <NowPlay />
+                    <hr />
+                    <h5>THIS WEEK OPEN</h5>
+                    <WeekMovie />
+                    <hr />
+                    <h5>COMIN SOON...</h5>
+                    <ComingSoon />
+                    <hr />
                 </div>
+                <Footer />
             </main>
         </div>
     );
