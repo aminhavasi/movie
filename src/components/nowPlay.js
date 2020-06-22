8055fae0d0ac8b5504898413b18776304d4c774b
amin havsi <pau.ahq@gmail.com>
2020-06-22 15:02:12 +0430

    final resize font on now play

--- src/components/nowPlay.js
+++ src/components/nowPlay.js
@@ -12,34 +12,68 @@ const NowPlay = () => {
                     <div className="row nowplay">
                         <div className="felx d-flex px-2 pt-3">
                             <i
-                                className="fa fa-star"
+                                className="fa fa-star mr-1"
                                 style={{ color: 'yellow' }}
                             />
                             <p>%76</p>
                         </div>
                         <div className="felx d-flex px-2 pt-3 ">
                             <i
-                                className="fa fa-film"
+                                className="d-none d-lg-block d-md-block fa fa-film mr-1"
                                 style={{ color: 'green' }}
                                 aria-hidden="true"
                             ></i>
-                            <p>%32</p>
+                            <p className="d-none d-lg-block d-md-block">%32</p>
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto  col-3">
-                    <img
-                        alt="img"
-                        className="img"
-                        src={require('./../resource/img/test.jpg')}
-                    />
-                </div>{' '}
                 <div className="mx-auto img-fluid col-3">
                     <img
                         alt="img"
                         className="img"
                         src={require('./../resource/img/test.jpg')}
                     />
+                    <div className="row nowplay">
+                        <div className="felx d-flex px-2 pt-3">
+                            <i
+                                className="fa fa-star mr-1"
+                                style={{ color: 'yellow' }}
+                            />
+                            <p>%76</p>
+                        </div>
+                        <div className="felx d-flex px-2 pt-3 ">
+                            <i
+                                className="d-none d-lg-block d-md-block fa fa-film mr-1"
+                                style={{ color: 'green' }}
+                                aria-hidden="true"
+                            ></i>
+                            <p className="d-none d-lg-block d-md-block">%32</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto img-fluid col-3">
+                    <img
+                        alt="img"
+                        className="img"
+                        src={require('./../resource/img/test.jpg')}
+                    />
+                    <div className="row nowplay">
+                        <div className="felx d-flex px-2 pt-3">
+                            <i
+                                className="fa fa-star mr-1"
+                                style={{ color: 'yellow' }}
+                            />
+                            <p>%76</p>
+                        </div>
+                        <div className="felx d-flex px-2 pt-3 ">
+                            <i
+                                className="d-none d-lg-block d-md-block fa fa-film mr-1"
+                                style={{ color: 'green' }}
+                                aria-hidden="true"
+                            ></i>
+                            <p className="d-none d-lg-block d-md-block">%32</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
