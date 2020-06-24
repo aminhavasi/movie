951f4ff252fe951be72cad9f5b9070f4af32ce8d
amin havsi <pau.ahq@gmail.com>
2020-06-24 22:13:51 +0430

    app.js edited

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NotFound from './components/common/notFound';
 import HomePage from './components/HomePage';
 import Register from './components/auth/register';
 import Dashboard from './components/admin/dashboard';
+import Login from './components/auth/login';
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route path="/register" component={Register} />
+                <Route path="/login" component={Login} />
                 <Route path="/admin" component={Dashboard} />
                 <Route path="/notFound" component={NotFound} />
                 <Redirect to="/notFound" />
