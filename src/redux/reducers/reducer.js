0f5f2b5e837b20704b7753957f4dd82aa65889db
amin havsi <pau.ahq@gmail.com>
2020-06-25 01:41:51 +0430

    login reducer

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -4,8 +4,11 @@ import {
     passwordRegister,
     usernameRegister,
 } from './registerReducer';
+import { emailLogin, passwordLogin } from './loginReducer';
 export const reducers = combineReducers({
     usernameRegister,
     emailRegister,
     passwordRegister,
+    emailLogin,
+    passwordLogin,
 });
