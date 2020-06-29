3b6255381ed6c468b08f20ec20fc2c3912698f8f
amin havasi <pau.ahq@gmail.com>
2020-06-30 02:30:08 +0430

    dasborad admin

--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -1,5 +1,19 @@
 import React from 'react';
-const Dashboard = () => {
-    return <div>admin</div>;
-};
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import NavbarAdmin from './navbarAdmin';
+import Sidebar from './sidebar';
+import MainAdmin from './admin';
+
+function Dashboard() {
+    return (
+        <div className="admin">
+            <NavbarAdmin />
+            <Sidebar />
+        </div>
+    );
+}
+
 export default Dashboard;
