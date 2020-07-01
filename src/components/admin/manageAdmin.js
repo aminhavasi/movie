5f34dbf0d6fad93501331eac620603b4f55f1c27
amin havasi <pau.ahq@gmail.com>
2020-07-01 04:43:17 +0430

    manage admin complicted

--- src/components/admin/manageAdmin.js
+++ src/components/admin/manageAdmin.js
@@ -1,36 +1,39 @@
 import './../../css/admin.css';
 import React from 'react';
 import getAdmins from './../services/fakeAdmins';
+import { useDispatch, useSelector } from 'react-redux';
+import { setId } from './../../redux/actions/adminAction';
 const ManageAdmins = () => {
     const admins = getAdmins();
+    const dispatch = useDispatch();
+    const adminId = useSelector((state) => state.setAdminSelect);
+    const handleClick = async (id) => {
+        await dispatch(setId(id));
+        await localStorage.setItem('userId', id);
+        window.location = '/admin/person';
+    };
     return (
-        // <div className=" bg-light shadow manageAdmin">
-        //     <div className="bg-dark text-white text-center">Manage Admins</div>
-        //     <div className="bg-danger m-2 p-2 adminUser">
-        //         <i className="fa fa-user mr-1 my-auto " />
-        //         <span>amin Hvasi</span>
-        //         <span className="ml-2"> employer</span>
-        //         <div className="pull-right ">
-        //             <span className="fa fa-trash my-auto mr-1 "></span>
-        //             <span className="fa fa-trash my-auto ml-4 "></span>
-        //         </div>
-        //     </div>
-        // </div>
-        <div className="container bg-light shadow manageAdmin">
-            <div className="bg-dark w-100 text-white text-center ">sas</div>
-            <div className="p-2 mt-1 mb-4  shadow">
-                <i className="fa fa-user mt-1  pull-left" />
-                <span className=""> amin havasi</span>
-                <span className="ml-2"> employer</span>
-                <span
-                    className="pull-right text-danger   fa fa-trash"
-                    style={{ fontSize: '20px' }}
-                ></span>
-                <span
-                    className="pull-right mr-3 text-warning fa fa-cog"
-                    style={{ fontSize: '20px' }}
-                ></span>
+        <div className=" bg-light shadow manageAdmin">
+            <div className="bg-dark w-100 text-white text-center ">
+                admin control page
             </div>
+            {admins.map((admin) => (
+                <div
+                    className="w-100 bg-light shadow mt-1 p-2 nav-link ok"
+                    key={admin.id}
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => handleClick(admin.id)}
+                >
+                    <div className="w-50 d-flex">
+                        <i className="fa fa-user my-auto" />
+                        <p className="my-auto ml-1">{admin.name}</p>
+                    </div>
+                    <div className="w-50 d-flex">
+                        <p className="my-auto">{admin.access}</p>
+                        <i className="fa fa-eye ml-auto my-auto" />
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
