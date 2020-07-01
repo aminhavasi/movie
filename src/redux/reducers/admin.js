8341917d96e4530f033dd4a3a52fcc01c8b1689b
amin havasi <pau.ahq@gmail.com>
2020-07-01 04:44:32 +0430

    reducer admin

---
+++ src/redux/reducers/admin.js
@@ -0,0 +1,20 @@
+export const setAdminSelect = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_ADMIN_SELECT':
+            return action.payload;
+        case 'CLEAR_ADMIN_SELCET':
+            return action.payload;
+        default:
+            return state;
+    }
+};
+export const setTest = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_TEST':
+            return action.payload;
+        case 'CLEAR_ADMIN_SELCET':
+            return action.payload;
+        default:
+            return state;
+    }
+};
